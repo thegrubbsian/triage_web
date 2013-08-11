40375a752ed4632a1652a4b60d26ee20bb751611
JC Grubbs <jc.grubbs@devmynd.com>
2013-08-11 12:00:45 -0500

    adding cors support for development

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   before_action :prepare_id_param
 
+  unless Rails.env.production?
+    before_action :cors_check
+    after_action :set_access_control_headers
+  end
+
   def render_errors(status, errors)
     render json: { errors: errors }, status: status
   end
@@ -21,6 +26,21 @@ class ApplicationController < ActionController::Base
 
   def prepare_id_param
     params[:id] = params[:id].to_i if params[:id].present?
+  end
+
+  def set_access_control_headers
+    headers["Access-Control-Allow-Origin"] = "*"
+    headers["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
+    headers["Access-Control-Max-Age"] = "1728000"
+  end
+
+  def cors_check
+    return unless request.method == :options
+    headers["Access-Control-Allow-Origin"] = "*"
+    headers["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
+    headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-Prototype-Version"
+    headers["Access-Control-Max-Age"] = "1728000"
+    render :text => "", :content_type => "text/plain"
   end
 
 end
