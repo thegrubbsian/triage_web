7b3cb60c214a2e0e290921af4fb21b82ce7c1c81
JC Grubbs <jc.grubbs@devmynd.com>
2013-08-22 13:43:45 -0500

    user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,18 @@
 class UsersController < ApplicationController
 
   def create
-    @user = User::Authenticator.sign_up(params[:user])
+    @user = User::Authenticator.sign_up(user_params)
     if @user
-      render :show
+      render json: @user
     else
       render_errors(500, { error_creating_user: "Could not create user." })
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 
 end
